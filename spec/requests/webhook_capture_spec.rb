2d753eb271b4e88f5f2f8ce0193fe8874ab84934
Van <singularity@vannio.co.uk>
2016-08-17 15:12:11 +0100

    Add more tests for edge cases :boom:

--- spec/requests/webhook_capture_spec.rb
+++ spec/requests/webhook_capture_spec.rb
@@ -1,9 +1,12 @@
 describe 'Webhook capture' do
-	it 'should raise an error with an empty webhook' do
-		expect{post('/emails')}.to raise_error('Error capturing webhook')
+
+	context 'with no webhook data' do
+		it 'should raise an error' do
+			expect{post('/emails')}.to raise_error('Error capturing webhook')
+		end
 	end
 
-	context 'with a functioning webhook' do
+	context 'with correct webhook data' do
 		let(:example_params) {
 			{
 				"Address":"barney@lostmy.name",
@@ -17,6 +20,27 @@ describe 'Webhook capture' do
 			expect{
 				post('/emails', params: example_params, as: :json)
 			}.to change{ Email.all.count }.by(1)
+		end
+	end
+
+	context 'with gibberish webhook data' do
+		let(:example_params) {
+			{
+				"wddw":"barney@lostmy.name",
+				"wqwe":"Shipment",
+				"Eveeeent":"send",
+				"Timeseeetamp":1432820696
+			}
+		}
+
+		it 'should not add entry to database' do
+			expect{
+				post('/emails', params: example_params, as: :json)
+			}.to_not change{ Email.all.count }
+		end
+
+		it 'should raise an error' do
+			expect{post('/emails')}.to raise_error('Error capturing webhook')
 		end
 	end
 end
