6162323f39e6ec4ebe2f48f11a80edf6de7327af
Van <singularity@vannio.co.uk>
2016-08-17 14:50:10 +0100

    Add test for failing webhook capture

--- spec/requests/webhook_capture_spec.rb
+++ spec/requests/webhook_capture_spec.rb
@@ -1,5 +1,11 @@
 describe 'Webhook capture' do
-	it 'should add entry to database' do
-		expect{post('/emails')}.to change{Email.all.count}.by(1)
+	it 'should raise an error with an empty webhook' do
+		expect{post('/emails')}.to raise_error('Error capturing webhook')
 	end
+
+	# scenario 'with a functioning webhook' do
+	# 	it 'should add entry to database' do
+	# 		expect{post('/emails')}.to change{Email.all.count}.by(1)
+	# 	end
+	# end
 end
