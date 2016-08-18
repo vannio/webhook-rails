2dd08ca5cf36f0a808473c1aa80e95f52fa48f13
Van <singularity@vannio.co.uk>
2016-08-18 15:11:03 +0100

    Add #calculate_rate to email model

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,11 @@
 class Email < ApplicationRecord
 	validates :address, :category, :event, :presence => true
+
+  def self.calculate_rate(category, event)
+    total = self.where(category: category).count
+    event = self.where(event: event, category: category).count
+
+    percentage = (event.to_f / total.to_f) * 100
+    percentage.nan? ? 0 : percentage
+  end
 end
