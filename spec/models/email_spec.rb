42529863300184bc6f802c4acb21d96e2f6cc5ab
Van <singularity@vannio.co.uk>
2016-08-18 15:00:20 +0100

    Add tests for calculating rates

---
+++ spec/models/email_spec.rb
@@ -0,0 +1,61 @@
+describe Email, :type => :model do
+  let(:example_emails) {[
+    { address:"barney@lostmy.name", category: "Shipment", event:"send" },
+    { address:"tom@lostmy.name", category: "UserConfirmation", event:"click" },
+    { address:"jacob@lostmy.name", category: "Order", event:"open" },
+    { address:"rehat@lostmy.name", category: "Shipment", event:"send" },
+    { address:"callum@lostmy.name", category: "ReferAFriend", event:"send" },
+    { address:"simon@lostmy.name", category: "Shipment", event:"send" },
+    { address:"josh@lostmy.name", category: "UserConfirmation", event:"open" }
+  ]}
+
+  before do
+    example_emails.each do |email|
+      Email.create(email)
+    end
+  end
+
+  context 'open' do
+    it 'should return the correct rate for Order' do
+      expect(described_class.calculate_rate('Order','open')).to eq(100);
+    end
+
+    it 'should return the correct rate for UserConfirmation' do
+      expect(described_class.calculate_rate('UserConfirmation','open')).to eq(50);
+    end
+
+    it 'should return the correct rate for Shipment' do
+      expect(described_class.calculate_rate('Shipment','open')).to eq(0);
+    end
+
+    it 'should return the correct rate for ReferAFriend' do
+      expect(described_class.calculate_rate('ReferAFriend','open')).to eq(0);
+    end
+
+    it 'should return the correct rate for GetABookDiscount' do
+      expect(described_class.calculate_rate('GetABookDiscount','open')).to eq(0);
+    end
+  end
+
+  # context 'click' do
+  #   it 'should return the correct rate for Order' do
+  #     expect(described_class.calculate_rate('Order','click')).to eq(100);
+  #   end
+  #
+  #   it 'should return the correct rate for UserConfirmation' do
+  #     expect(described_class.calculate_rate('UserConfirmation','click')).to eq(0);
+  #   end
+  #
+  #   it 'should return the correct rate for Shipment' do
+  #     expect(described_class.calculate_rate('Shipment','click')).to eq(0);
+  #   end
+  #
+  #   it 'should return the correct rate for ReferAFriend' do
+  #     expect(described_class.calculate_rate('ReferAFriend','click')).to eq(0);
+  #   end
+  #
+  #   it 'should return the correct rate for GetABookDiscount' do
+  #     expect(described_class.calculate_rate('GetABookDiscount','click')).to eq(0);
+  #   end
+  # end
+end
