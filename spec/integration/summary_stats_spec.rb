b4a64f6cda415f6755eba8cd821b1fb095e5a7c5
Van <singularity@vannio.co.uk>
2016-08-18 15:44:10 +0100

    Add feature tests for rate lists

--- spec/integration/summary_stats_spec.rb
+++ spec/integration/summary_stats_spec.rb
@@ -1,6 +1,9 @@
 feature 'Summary stats' do
-	scenario 'should display a stats table' do
-		visit('/emails')
+  before do
+    visit('/emails')
+  end
+
+  scenario 'should display a stats table' do
 		within_table('summary') do
 			expect(page).to have_content('Total number of emails sent')
 			expect(page).to have_content('Total number of emails opened')
@@ -9,4 +12,29 @@ feature 'Summary stats' do
 			expect(page).to have_content('Click rate per email type')
 		end
 	end
+
+  scenario 'should display rates in an unordered list' do
+    email_types = [
+      "Order",
+  		"UserConfirmation",
+  		"Shipment",
+  		"ReferAFriend",
+  		"GetABookDiscount"
+    ]
+
+    within_table('summary') do
+      open_rate_cell = find('td', text: 'Open rate per email type').find('+td')
+      click_rate_cell = find('td', text: 'Click rate per email type').find('+td')
+
+      within(open_rate_cell) do
+        expect(page).to have_css('ul')
+        expect(page).to have_css("li:nth-child(#{email_types.length})")
+      end
+
+      within(click_rate_cell) do
+        expect(page).to have_css('ul')
+        expect(page).to have_css("li:nth-child(#{email_types.length})")
+      end
+    end
+  end
 end
