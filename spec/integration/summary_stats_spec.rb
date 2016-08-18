99ccad4021862afab06cde1784a53b56f56911b6
Van <singularity@vannio.co.uk>
2016-08-18 14:00:28 +0100

    Add some more tests for the other total counts

--- spec/integration/summary_stats_spec.rb
+++ spec/integration/summary_stats_spec.rb
@@ -26,9 +26,21 @@ feature 'Summary stats' do
 		end
 	end
 
-  scenario 'should display correct total sent emails' do
+  scenario 'should display correct total number of sent emails' do
     visit('/emails')
     sent_cell = find('td', text: 'Total number of emails sent').find('+td')
     expect(sent_cell).to have_content(4)
+  end
+
+  scenario 'should display correct total number of opened emails' do
+    visit('/emails')
+    opened_cell = find('td', text: 'Total number of emails opened').find('+td')
+    expect(opened_cell).to have_content(2)
+  end
+
+  scenario 'should display correct total number of clicks' do
+    visit('/emails')
+    clicked_cell = find('td', text: 'Total number of clicks').find('+td')
+    expect(clicked_cell).to have_content(1)
   end
 end
